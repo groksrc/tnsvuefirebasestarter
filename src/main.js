81b33e1338b86713925129d2f3dd07b1d90bd7a6
Drew Cain <drew@simpli.fi>
2018-06-28 22:39:11 -0500

    import firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'nativescript-vue';
+import firebase from 'nativescript-plugin-firebase'
 
 import router from './router';
 
 import store from './store';
 
 import './styles.scss';
+
+firebase.init({
+  onAuthStateChanged: data => { // optional
+    console.log((data.loggedIn ? "Logged in to firebase" : "Logged out from firebase") + " (init's onAuthStateChanged callback)");
+  },
+  persist: false,
+}).then(
+  () => {
+    console.log("Firebase is ready");
+  },
+  error => {
+    console.log("firebase.init error: " + error);
+  }
+);
 
 // Uncommment the following to see NativeScript-Vue output logs
 //Vue.config.silent = false;
